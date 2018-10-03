9bba39a55dd9032dc7222ad400498c6506f0f207
Nele Taevere <ntaevere@gmail.com>
2018-10-03 16:24:34 +1300

    create array of characters

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//Get the element with the ID of qwerty and save it to a variable.
 const qwerty = document.querySelector('#qwerty');
+//Get the element with the ID of phrase and save it to a variable.
 const phrase = document.querySelector('#phrase');
+//Create a missed variable, initialized to 0
 const missed = 0;
 const startGameButton = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
+//Create a phrases array that contains at least 5 different phrases as strings
 const phrases = [
   'I am pretending to be a tomato',
   'Tomorrow has been cancelled due to lack of interest',
@@ -14,8 +18,34 @@ const phrases = [
 startGameButton.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
-
-function getRandomPhraseArray() {
-  let random = Math.floor(Math.random() * (phrases.length));
-  document.phrase.innerHTML = phrases[random];
+//Create a getRandomPhraseAsArray function
+function getRandomPhraseArray(arr) {
+  //Randomly choose a phrase
+  let random = phrases[Math.floor(Math.random() * phrases.length)];
+  //Split that phrase into a new array of characters
+  let arrayOfCharacters = random.split('');
+  //Return the new character array
+  return arrayOfCharacters;
 }
+  //Create an addPhraseToDisplay function
+function addPhraseToDisplay(arr) {
+  //Loop through an array of characters
+for (let i = 0; i < arrayOfCharacters.length; i += 1) {
+  //for each character in the array, you’ll create a list item, put the character inside of the list item
+  const li = arrayOfCharacters[i];
+  //Append that list item to the #phrase ul in your HTML
+  const div = document.getElementById('phrase');
+  const ul = div.getElementsByTagName('ul');
+  ul.appendChild(li);
+  //If the character in the array is a letter and not a space, the function should add the class “letter” to the list item
+  if (li !== '') {
+    li.className = 'letter';
+  } else {
+    li.className = '';
+  }
+}
+}
+// Pass in the phrases array as an argument when you call the function
+const phraseArray = getRandomPhraseArray(phrases);
+addPhrasetoDisplay(phraseArray);
+
