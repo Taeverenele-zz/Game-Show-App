11879e9743f66e78a027542fddec91247c61eebc
Nele Taevere <ntaevere@gmail.com>
2018-10-05 09:04:38 +1300

    modify game reset functionality

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
+// Variables
+const title = document.querySelector('.title');
 const startGameButton = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
 const letters = document.getElementsByClassName('letter');
 const lettersFound = document.getElementsByClassName('show');
-const title = document.querySelector('.title');
+const ul = document.querySelector('#phrase ul');
+const keyrow = document.querySelectorAll('.keyrow button');
 let missed = 0;
 const phrases = [
   'I am pretending to be a tomato',
@@ -14,21 +17,44 @@ const phrases = [
   'Never put a cat on your head'
 ];
 
+// Event Handlers
 startGameButton.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
+qwerty.addEventListener('click', (e) => {
+  let letterChosen = e.target;;
+if (e.target.tagName === 'BUTTON') {
+  letterChosen.className = 'chosen';
+  letterChosen.disabled = true;
+  letterChosen = checkLetter(e.target);
+  if (letterChosen === null) {
+    document.querySelector('.tries').remove();
+    missed++;
+  }
+}
+checkWin();
+});
+startGameButton.addEventListener('click', (e) => {
+  if (startGameButton.textContent === 'Play again' || startGameButton.textContent === 'Try again') {
+    missed = 0;
+    ul.innerHTML = '';
+    for (let i = 0; i < keyrow.length; i += 1) {
+      keyrow[i].className = '';
+      keyrow[i].disabled = false;
+    }
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhraseToDisplay(phraseArray);
+  }
+})
 
-function getRandomPhraseAsArray(arr) {
+// Functions
+const getRandomPhraseAsArray = (arr) => {
   const randomPhrase = arr[Math.floor(Math.random() * arr.length)];
   const characters = randomPhrase.split('');
   return characters;
 }
-
-const phraseArray = getRandomPhraseAsArray(phrases);
-
-function addPhraseToDisplay(arr) {
+const addPhraseToDisplay = (arr) => {
   for (let i = 0; i < arr.length; i += 1) {
-    const ul = document.querySelector('#phrase ul');
     const li = document.createElement('li');
     const character = arr[i];
     li.textContent = character;
@@ -40,9 +66,7 @@ function addPhraseToDisplay(arr) {
     }
   }
 }
-addPhraseToDisplay(phraseArray);
-
-function checkLetter(button) {
+const checkLetter = (button) => {
   let guess = null;
   for (let i = 0; i < letters.length; i += 1) {
     if (button.textContent == letters[i].textContent.toLowerCase()) {
@@ -52,7 +76,7 @@ function checkLetter(button) {
   }
   return guess;
 }
-function checkWin() {
+const checkWin = () => {
   if (lettersFound.length === letters.length) {
    overlay.style.display = 'flex';
    overlay.className = 'win';
@@ -66,30 +90,7 @@ function checkWin() {
   }
  }
 
- qwerty.addEventListener('click', (e) => {
-  let letterChosen = e.target;;
-  if (e.target.tagName === 'BUTTON') {
-    letterChosen.className = 'chosen';
-    letterChosen.disabled = true;
-    letterChosen = checkLetter(e.target);
-    if (letterChosen === null) {
-      document.querySelector('.tries').remove();
-      missed++;
-    }
-  }
-  checkWin();
-});
+// Calling functions
+ const phraseArray = getRandomPhraseAsArray(phrases);
+ addPhraseToDisplay(phraseArray);
 
-
-//Add a button to the “success” and “failure” screens that reset the game.
-
-startGameButton.addEventListener('click', (e) => {
-  if (startGameButton.textContent === 'Play again' || startGameButton.textContent === 'Try again') {
-    // recreate the buttons in the keyboard
-    // generate a new random phrase
-    const phraseArray = getRandomPhraseAsArray(phrases);
-    addPhraseToDisplay(phraseArray);
-    // set the number of missed to zero
-    missed = 0;
-  }
-})
