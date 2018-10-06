53d32658251a2b245d89d71104b9af10f6086457
Nele Taevere <ntaevere@gmail.com>
2018-10-06 14:43:03 +1300

    refactored

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Variables
 const startGameButton = document.querySelector('.btn__reset');
+const playAgainButtonText = 'Play again';
+const tryAgainButtonText = 'Try again';
 const overlay = document.getElementById('overlay');
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
@@ -7,6 +9,7 @@ const letters = document.getElementsByClassName('letter');
 const lettersFound = document.getElementsByClassName('show');
 const ul = document.querySelector('#phrase ul');
 const hearts = document.querySelectorAll('.tries img');
+const title = document.querySelector('.title');
 let missed = 0;
 const phrases = [
   'I am pretending to be a tomato',
@@ -16,26 +19,27 @@ const phrases = [
   'Never put a cat on your head'
 ];
 
-// Event Handlers
+// Add event listeners
 startGameButton.addEventListener('click', () => {
   overlay.style.display = 'none';
 });
-qwerty.addEventListener('click', (e) => {
-  let letterChosen = e.target;
-    if (e.target.tagName === 'BUTTON') {
-      letterChosen.className = 'chosen';
-      letterChosen.disabled = true;
-      letterChosen = checkLetter(e.target);
-      if (letterChosen === null) {
-        i = missed;
-        hearts[i].setAttribute('src', 'images/lostHeart.png');
-        missed++;
-      }
+
+qwerty.addEventListener('click', e => {
+  let clickedElement = e.target;
+  if (clickedElement.tagName === 'BUTTON') {
+    clickedElement.className = 'chosen';
+    clickedElement.disabled = true;
+    const matches = checkLetter(clickedElement.textContent);
+    if (!matches) {
+      hearts[missed].setAttribute('src', 'images/lostHeart.png');
+      missed++;
     }
+  }
   checkWin();
 });
-startGameButton.addEventListener('click', () => {
-  if (startGameButton.textContent === 'Play again' || startGameButton.textContent === 'Try again') {
+
+startGameButton.addEventListener('click', e => {
+  if (e.target.textContent === playAgainButtonText || e.target.textContent === tryAgainButtonText) {
     const keyrow = document.querySelectorAll('.keyrow button');
     missed = 0;
     ul.innerHTML = '';
@@ -52,46 +56,54 @@ startGameButton.addEventListener('click', () => {
 })
 
 // Functions
-const getRandomPhraseAsArray = (arr) => {
+const getRandomPhraseAsArray = arr => {
   const randomPhrase = arr[Math.floor(Math.random() * arr.length)];
   return randomPhrase.split('');
 }
-const addPhraseToDisplay = (arr) => {
+
+const addPhraseToDisplay = arr => {
   for (let i = 0; i < arr.length; i += 1) {
     const li = document.createElement('li');
     const character = arr[i];
     li.textContent = character;
     ul.appendChild(li);
-    if (character !== ' ') {
-      li.className = 'letter';
-    } else {
-      li.className = 'space';
-    }
+    const isSpaceCharacter = character === ' ';
+    li.className = isSpaceCharacter ? 'space' : 'letter';
   }
 }
-const checkLetter = (button) => {
+
+const checkLetter = letter => {
   let guess = null;
   for (let i = 0; i < letters.length; i += 1) {
-    if (button.textContent == letters[i].textContent.toLowerCase()) {
-      guess = button.textContent;
+    if (letter == letters[i].textContent.toLowerCase()) {
+      guess = letter;
       letters[i].classList.add('show');
     }
   }
   return guess;
 }
 
+const playerHasWon = () => {
+  overlay.style.display = 'flex';
+  overlay.className = 'win';
+  title.textContent = 'You have won!!! Wooohhoooo!!!!';
+  startGameButton.textContent = playAgainButtonText;
+}
+
+const playerHasLost = () => {
+  overlay.style.display = 'flex';
+  overlay.className = 'lose';
+  title.textContent = 'Game over! You lose!';
+  startGameButton.textContent = tryAgainButtonText;
+}
+
 const checkWin = () => {
-  const title = document.querySelector('.title');
   if (lettersFound.length === letters.length) {
-   overlay.style.display = 'flex';
-   overlay.className = 'win';
-   title.textContent = 'You have won!!! Wooohhoooo!!!!';
-   startGameButton.textContent = 'Play again';
-  } if (missed === 5 ) {
-   overlay.style.display = 'flex';
-   overlay.className = 'lose';
-   title.textContent = 'Game over! You lose!';
-   startGameButton.textContent = 'Try again';
+    playerHasWon();
+    return;
+  }
+  if (missed === 5 ) {
+    playerHasLost();
   }
  }
 
@@ -101,14 +113,13 @@ const checkWin = () => {
 
 /////////////// Alternative game-reset ///////////////////
 
-//  window.addEventListener('click', event => {
-//   if ( event.target.innerHTML === 'Play again') {
-//     reset();
-//   } else if ( event.target.innerHTML === 'Try again') {
+//  window.addEventListener('click', e => {
+  // const isPlayAgainOrTryAgain = e.target.innerHTML === playAgainButtonText || e.target.innerHTML === tryAgainButtonText;
+//   if (isPlayAgainOrTryAgain) {
 //     reset();
 //   }
 // });
 
-// function reset() {
+// const reset = () => {
 //  window.location.reload(true);
 // }
